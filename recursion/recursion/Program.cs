9bf3495dfe7594a15a091242cf1126f38366f3bb
Melancholy <mrviduus@gmail.com>
2018-09-03 17:33:53 +0300

    comit0309

--- recursion/recursion/Program.cs
+++ recursion/recursion/Program.cs
@@ -29,8 +29,50 @@ namespace recursion
     }
     //Factorial
 
+    //bashni hanoja
+    static void hanoi(int x, char from, char to, char help)
+    {
+      if (x > 0)
+      {
+        hanoi(x - 1, from, help, to);
+        move(x, from, to);
+        hanoi(x - 1, help, to, from);
+      }
+
+    }
+
+    static void move(int x, char from, char to)
+    {
+      Console.WriteLine("  берем диск " + x + " из " + from + " в " + to);
+    }
+
+
+    //bashni hanoja
+
+
+
     static void Main(string[] args)
     {
+      int x;
+      char from = 'A', to = 'B', help = 'C';
+
+      do
+      {
+        try
+        {
+          Console.Write("Введите количество дисков: ");
+          x = Int32.Parse(Console.ReadLine());
+        }
+        catch (FormatException e)
+        {
+          x = -10;
+        }
+      } while (x == -10 || x > 10);
+      Console.WriteLine("\n  откуда = A, куда = B, вспомогательная колона = C\n");
+      hanoi(x, from, to, help);
+      //basni hanoja
+
+
       Console.WriteLine("Enter number for factorial: ");
       int n = int.Parse(Console.ReadLine());
       Console.WriteLine("Factorial is: {0}", Factorial(n));
