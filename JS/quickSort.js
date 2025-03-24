5f3a4a251ad0a52c177c04f4cbdce4cefa7398e3
Vasyl Vdovychenko <mrviduus@gmail.com>
2025-03-23 20:08:51 -0400

    Add quick sort implementation

---
+++ JS/quickSort.js
@@ -0,0 +1,32 @@
+function pivot(arr, start=0, end = arr.length+1){
+  function swap(array, i, j){
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  var pivot = arr[start];
+  var swapIndex = start;
+
+  for(var i = start + 1; i < arr.length; i ++){
+    if(pivot > arr[i]){
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, start, swapIndex);
+  return swapIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1){
+  if(left < right){
+    let pivotIndex = pivot(arr, left, right);
+    //left
+    quickSort(arr, left, pivotIndex -1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+console.log(pivot([4,8,2,1,5,7,6,3]));
+console.log(quickSort([4,8,2,1,5,7,6,3]));
