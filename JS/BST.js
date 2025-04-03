6016f4d73efe01b95fec5f31e22adc93ea776308
Vasyl Vdovychenko <mrviduus@gmail.com>
2025-04-03 16:00:44 -0400

    DFS in order

--- JS/BST.js
+++ JS/BST.js
@@ -53,16 +53,69 @@ class BinarySearchTree{
     if(!found) return undefined;
     return current;
   }
+  BFS(){
+    var node = this.root,
+        data = [],
+        queue = [];
+    queue.push(node);
+    while(queue.length){
+      node = queue.shift();
+      data.push(node.value);
+      if(node.left) queue.push(node.left);
+      if(node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder(){
+    var data = [];
+    var current = this.root;
+    function traverse (node){
+      data.push(node.value);
+      if(node.left){
+        traverse (node.left);
+      }
+      if(node.right){
+        traverse(node.right);
+      }
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSPostOrder(){
+    var data = [];
+    function traverse(node){
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSInOrder(){
+    var data = [];
+    function traverse(node){
+      if(node.left) traverse(node.left);
+      data.push(node.value);
+      if(node.right) traverse(node.right);
+      
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 var tree = new BinarySearchTree();
 tree.root = new Node(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 console.log(tree);
-console.log(tree.find(5));
+console.log(tree.find(20));
 console.log(tree.find(100));
+console.log(tree.BFS());
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
+
